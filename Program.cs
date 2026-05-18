1002a7c76f226f07ae2b28e0f2ab04a1c8760bfe
elehmzzz <elehmzzz@gmail.com>
2026-05-18 11:29:08 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Materias.ejemplo1;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+
+namespace Materias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Materia matematicas = new Materia();
+            matematicas._p1 = 10;
+            matematicas._p2 = 8;
+            matematicas._p3 = 10;
+            Console.WriteLine(matematicas.calPromedio());
+        }
+    }
+}
---
+++ ejemplo1/Materia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materias.ejemplo1
+{
+    public class Materia
+    {
+        private float p1;
+        private float p2;
+        private float p3;
+        private string nombreMateria;
+
+        //propiedades 
+        public float _p1 {
+            get { return p1; }
+            set { _p1 = value; }
+        }
+        public float _p2 {
+            get { return p2; }
+            set { p2 = value; }
+        }
+        public float _p3 {
+            get { return p3; }
+            set { p3 = value; }
+        }
+
+        //metodo
+        public float calPromedio() {
+            float prom = 0;
+            prom = (p1 + p2 + p3) / 3;
+            return prom;
+        }
+    }
+}
